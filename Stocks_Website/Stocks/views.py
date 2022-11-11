aa26dcdf25c89f63766256c218e85978abcb5b89
P4ITHV1 <108858835+P4ITHV1@users.noreply.github.com>
2022-11-11 22:19:08 +0530

    Add files via upload

---
+++ Stocks_Website/Stocks/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+def index(request):
+    return render(request, 'index.html')
+
+def stock(request):
+    return render(request, 'stock.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = "profile.html"
